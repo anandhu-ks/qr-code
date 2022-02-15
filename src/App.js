8cc30a02291c3c93ea333a37630db282e24b7d91
Anandhu KS <anandhu.ks@bankopen.co>
2022-02-15 23:30:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./main.css";
+import QRCode from "qrcode";
 
 function App() {
+  const [generateDisabled, setGenerate] = useState(true);
+  const [download_enabled, setDownload] = useState(false);
+  const [text, setText] = useState("");
+  const [url, setUrl] = useState("");
+  const [show_image, setImage] = useState(true);
+
+  useEffect(() => {
+    const toggleGenerate = () => {
+      if (text.length) {
+        setGenerate(false);
+      } else {
+        setGenerate(true);
+      }
+    };
+    toggleGenerate();
+  }, [text]);
+
+  const generateQR = async () => {
+    const res = await QRCode.toDataURL(text);
+    setUrl(res);
+    setDownload(true);
+    setImage(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <input
+          onChange={(e) => {
+            setText(e.target.value);
+            setDownload(false);
+            setImage(false);
+          }}
+          placeholder="Enter your text here"
+          type="text"
+        ></input>
+        <div className="buttons">
+          <button
+            onClick={() => {
+              generateQR();
+            }}
+            disabled={generateDisabled}
+          >
+            Generate QR
+          </button>
+          {download_enabled && (
+            <a href={url} download>
+              Download
+            </a>
+          )}
+        </div>
+        {show_image && (
+          <div className="qr-image">
+            <img src={url} alt=""></img>
+          </div>
+        )}
+      </div>
+      <div className="footer">Powered by OCTOPUS MEDIA</div>
     </div>
   );
 }
